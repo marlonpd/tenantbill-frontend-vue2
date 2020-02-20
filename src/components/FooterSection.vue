fc929baa23adae7185c9ff3566714ea4780ab122
marlonpd <send8heremd@gmail.com>
2020-02-20 13:43:59 +0800

    Add app scafolding

---
+++ src/components/FooterSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="container">
+      <router-link class="logo-font" :to="{ name: 'home', params: {} }"> 
+      </router-link>
+      <span class="attribution">
+        Ans interactive learning project froms
+        <a rel="noopener noreferrer" target="blank" href="https://thinkster.io"
+          ></a
+        >. Code &amp; design licensed under MIT.
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterSection"
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign insss</h1>
+          <p class="text-xs-center">
+            <router-link :to="{ name: 'register' }">
+              Need an account?s
+            </router-link>
+          </p>
+          <form >
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                v-model="email"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                v-model="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: null,
+      password: null
+    };
+  },
+  methods: {
+  
+  },
+  computed: {
+    ...mapState({
+      errors: state => state.auth.errors
+    })
+  }
+};
+</script>

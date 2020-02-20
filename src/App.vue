bbd44076cfefd5746f2e6d310a6d54a90d1f69e7
marlonpd <send8heremd@gmail.com>
2020-02-20 13:43:05 +0800

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderSection />
+    <router-view></router-view>
+    <FooterSection />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderSection from "@/components/HeaderSection";
+import FooterSection from "@/components/FooterSection";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderSection,
+    FooterSection
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
 Vue.config.productionTip = false
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+router.push({
+  name: 'login'
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')

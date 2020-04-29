bcad93e36e556ed70187bb2418cb23cbecc7db28
marlonpd <send8heremd@gmail.com>
2020-04-29 09:09:32 +0800

    Add powerrates and profile views

---
+++ src/views/PowerRates.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div  class="row m-auto">
+           Power Rates
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { FETCH_TENANTS } from "@/store/actions.type";
+
+export default {
+  name: "PowerRates",
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    this.fetchTenants();
+  },
+  methods: {
+    fetchTenants() {
+      this.$store.dispatch(FETCH_TENANTS);
+    }
+  },
+  computed: {
+    ...mapGetters(['allTenats']),
+  }
+};
+</script>
+
+<style >
+.error-messages {
+  -webkit-padding-start: 0;
+  list-style-type: none;
+  color: red;
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div  class="row m-auto">
+           Profile
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Profile",
+  data() {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  },
+  computed: {
+    ...mapGetters(['allTenats']),
+  }
+};
+</script>
+
+<style >
+
+</style>
